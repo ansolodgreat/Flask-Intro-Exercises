d229e3c83b523f2f4a708bbba4edcaa3374b8a8c
JM Dumlao <ansolo.dumlao@gmail.com>
2024-06-04 22:26:10 -0400

    modified

--- calc/app.py
+++ calc/app.py
@@ -1,18 +1,39 @@
-from flask import Flask
+from flask import Flask, request
+from operations import add, sub, mult, div
+
 app = Flask(__name__)
 
 
-@app.route("/welcome")
-def welcome():
-    return "welcome"
+@app.route('/add')
+def do_add():
+    a = int(request.args.get('a'))
+    b = int(request.args.get('b'))
+    result = add(a,b)
+    return str(result)
 
-@app.route("/welcome/home")
-def welcome_home():
-    return "welcome home"
 
-@app.route("/welcome/back")
-def welcome_back():
-    return "welcome back"
+@app.route('/sub')
+def do_sub():
+    a = int(request.args.get('a'))
+    b = int(request.args.get('b'))
+    result = sub(a,b)
+    return str(result)
+
+
+@app.route('/mult')
+def do_mult():
+    a = int(request.args.get('a'))
+    b = int(request.args.get('b'))
+    result = mult(a,b)
+    return str(result)
+
+
+@app.route('/div')
+def do_div():
+    a = int(request.args.get('a'))
+    b = int(request.args.get('b'))
+    result = div(a,b)
+    return str(result)
 
 if __name__ == '__main__':
     app.run(debug=True)
